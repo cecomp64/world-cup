16f420f56ef5a261554f2db622f359979cf75dcc
Carl Svensson <csvenss2@gmail.com>
2014-06-22 10:03:39 -0700

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WorldCup::Application.config.secret_key_base = '4f942804140d8e56b37002083a07942781a3c56c0f905440fd00890291aab4016375976a90688b154c078c884f17dc7e48869649d1e3a44013c329cc95928b9e'
